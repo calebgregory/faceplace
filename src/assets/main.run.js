fdef081cf0cd18f7e2e291fd69249f8a6cd117e4
calebgregory <calebgregory@gmail.com>
2015-06-12 14:38:12 -0500

    Removing the ngStorage stuff

--- src/assets/main.run.js
+++ src/assets/main.run.js
@@ -1,10 +1,10 @@
 angular
   .module('faceplace')
-  .run(function($rootScope, $location, Auth) {
+  .run(function($rootScope, Auth) {
+    //$scope.$storage = $localStorage;
     $rootScope
       .$on('$routeChangeStart', function(event, nextRoute) {
         if(nextRoute.$$route && nextRoute.$$route.private) {
-          //$scope.$storage = $localStorage;
           Auth.requireLogin();
         }
       });
